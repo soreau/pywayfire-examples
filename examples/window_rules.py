9a73b987f33d21abef9f04f3ec67045d3c942118
killown <systemofdown@gmai.com>
2024-08-06 20:23:52 -0300

    new example

--- examples/window_rules.py
+++ examples/window_rules.py
@@ -3,19 +3,20 @@ rule = WayfireSocket()
 rule.watch()
 
 view = None
-
+id = None
 def match_view(app_id):
     if view["app-id"] == app_id:
         return True
 
 #build your window rules here, the setup is just here
 def window_rules(view):
+    id = view["id"]
     if match_view("kitty"):
-        rule.set_view_always_on_top(view["id"], True)
-        rule.set_view_fullscreen(view["id"])
+        rule.set_view_always_on_top(id, True)
+        rule.set_view_fullscreen(id)
     if match_view("Picture-in-Picture"):
-        rule.set_view_always_on_top(view["id"], True)
-        rule.set_workspace({"x":0, "y":1}, view["id"])
+        rule.set_view_always_on_top(id, True)
+        rule.set_workspace({"x":0, "y":1}, id)
         rule.set_workspace({"x":0, "y":0})
 
 #you dont need to touch this
