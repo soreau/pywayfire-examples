47af037b1a407753d31bd51fd0ae1e0c250752d9
killown <systemofdown@gmai.com>
2024-08-06 22:39:31 -0300

    new example

---
+++ examples/disable_plugin_on_fullscreen.py
@@ -0,0 +1,28 @@
+from wayfire import WayfireSocket
+sock = WayfireSocket()
+
+plugins = sock.get_option_value("core/plugins")["value"]
+plugin_name_to_disable = "pixdecor"
+
+def disable_plugin(plugin_name):
+    p = " ".join([i for i in plugins.split() if plugin_name not in i])
+    s = WayfireSocket()
+    s.set_option_values({"core/plugins":p})
+
+def enable_plugin():
+    s = WayfireSocket()
+    s.set_option_values({"core/plugins":plugins})
+
+
+sock.watch()
+while True:
+    msg = sock.read_message()
+    if "event" in msg and "view" in msg:
+        view = msg["view"]
+        if view:
+            view = sock.get_focused_view()
+            if view["fullscreen"] is True:
+                disable_plugin(plugin_name_to_disable)
+            if view["fullscreen"] is False:
+                enable_plugin()
+
