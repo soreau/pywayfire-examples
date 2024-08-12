2f96eedf0f7ca840529a0b5cd4089384c04cd5f2
killown <systemofdown@gmai.com>
2024-08-12 03:21:32 -0300

    new example [backup]

---
+++ examples/shader_focused_view_except_single.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+# A script to apply a shader to all views except the active view
+# When a new view is focused, the shader is applied to the previously
+# active view. It skips app-id "panel", as this is wf-panel's app-id.
+
+import os
+import sys
+from wayfire import WayfireSocket
+from wayfire.extra.wpe import WPE
+from wayfire.extra.ipc_utils import WayfireUtils
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} /path/to/filters/shader")
+    sys.exit(-1)
+
+shader_path = os.path.abspath(sys.argv[1])
+
+sock = WayfireSocket()
+wpe = WPE(sock)
+utils = WayfireUtils(sock)
+sock.watch(['view-focused'])
+
+def has_workspace_single_view():
+    views_ac = utils.get_views_from_active_workspace()
+    views_ac = [sock.get_view(view_id) for view_id in views_ac]
+    toplevel_views = [view for view in views_ac if view["role"] == "toplevel" and view["mapped"]]
+    return len(toplevel_views) == 1
+
+def unset_view_shaders():
+    for view in sock.list_views():
+        wpe.filters_unset_view_shader(view["id"])
+
+
+while True:
+    try:
+        msg = sock.read_next_event()
+        if "event" in msg:
+            if has_workspace_single_view():
+                continue
+            unset_view_shaders()
+            focused_view = sock.get_focused_view()
+            if focused_view:
+                wpe.filters_set_view_shader(focused_view["id"], "/home/neo/border")
+    except KeyboardInterrupt:
+        unset_view_shaders()
+        sys.exit(0)
+
