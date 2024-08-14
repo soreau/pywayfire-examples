34cc37db0cd328b89bc7b03e2c3a537dbc290a45
killown <systemofdown@gmai.com>
2024-08-14 14:37:09 -0300

    new example

--- examples/set_view_as_wallpaper.py
+++ examples/set_view_as_wallpaper.py
@@ -5,5 +5,4 @@ wpe = WPE(sock)
 
 focused_view_id = sock.get_focused_view()["id"]
 wpe.pin_view(focused_view_id, "bottom", False, 0, 0)
-sock.set_view_sticky(focused_view_id, True)
 sock.set_view_fullscreen(focused_view_id, True)
