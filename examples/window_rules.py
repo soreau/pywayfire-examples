cc76d8e1e53b20d241aa3e1afc535bd40b741020
killown <systemofdown@gmai.com>
2024-08-18 22:08:19 -0300

    new example

--- examples/window_rules.py
+++ examples/window_rules.py
@@ -1,29 +1,80 @@
 from wayfire import WayfireSocket
-rule = WayfireSocket()
-rule.watch()
 
-view = None
-id = None
-def match_view(app_id):
-    if view["app-id"] == app_id:
-        return True
+class AdvancedWindowRules:
+    def __init__(self):
+        self.rule = WayfireSocket()
+        self.rule.watch(["event"])
+        self.view = None
+        self.id = None
 
-#build your window rules here, the setup is just here
-def window_rules(view):
-    id = view["id"]
-    if match_view("kitty"):
-        rule.set_view_always_on_top(id, True)
-        rule.set_view_fullscreen(id)
-    if match_view("Picture-in-Picture"):
-        rule.set_view_always_on_top(id, True)
-        rule.set_workspace({"x":0, "y":1}, id)
-        rule.set_workspace({"x":0, "y":0})
+    def match_view(self, app_id):
+        return self.view and self.view["app-id"] == app_id
 
-#you dont need to touch this
-while True:
-    msg = rule.read_message()
-    if "event" in msg and "view" in msg:
-        view = msg["view"]
-        if view:
-            window_rules(view)
+    def view_mapped(self, msg):
+        # When a new view is created (mapped)
+        self.view = msg["view"]
+        self.id = self.view["id"]
+
+        if self.match_view("kitty"):
+            self.rule.set_view_fullscreen(self.id, True)
+
+        if self.match_view("Picture-in-Picture"):
+            self.rule.set_view_always_on_top(self.id, True)
+            self.rule.set_workspace({"x": 0, "y": 1}, self.id)
+            self.rule.set_workspace({"x": 0, "y": 0})
+
+    def focused_view_rule(self, msg):
+        focused_view = msg["view"]
+        focused_view_id = focused_view["id"]
+
+        if self.match_view("kitty"):
+            self.rule.set_view_fullscreen(focused_view_id, True)
+
+    def output_gain_focus(self, msg):
+        # Add code here to handle when a new output (monitor) gains focus
+        pass
+
+    def plugin_activation_state_changed(self, msg):
+        # Add code here to handle when a plugin is activated or deactivated
+        pass
+
+    def view_unmapped(self, msg):
+        # Add code here to handle when a view is closed (unmapped)
+        pass
+
+    def view_workspace_changed(self, msg):
+        # Add code here to handle when a view changes its workspace
+        pass
+
+    def view_geometry_changed(self, msg):
+        # Add code here to handle when a view's geometry changes
+        pass
+
+    def handle_events(self, msg):
+        event = msg["event"]
+
+        if event == "focused_view":
+            self.focused_view_rule(msg)
+        elif event == "output-gain-focus":
+            self.output_gain_focus(msg)
+        elif event == "plugin-activation-state-changed":
+            self.plugin_activation_state_changed(msg)
+        elif event == "view-mapped":
+            self.view_mapped(msg)
+        elif event == "view_unmapped": 
+            self.view_unmapped(msg)
+        elif event == "wset-workspace-changed":
+            self.view_workspace_changed(msg)
+        elif event == "view-geometry-changed":
+            self.view_geometry_changed(msg)
+
+    def run(self):
+        while True:
+            msg = self.rule.read_message()
+            if "event" in msg:
+                self.handle_events(msg)
+
+# Instantiate and run the window rules
+window_rules = AdvancedWindowRules()
+window_rules.run()
 
